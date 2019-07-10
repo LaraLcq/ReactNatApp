49c50d9e2870e95cd1f20b2bd4b89e0d3474180e
laralcq <laraloicq@gmail.com>
2019-07-10 12:32:53 +0200

    Input Text ok

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native';
-import FilmItem from './FilmItem'
-import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' 
+// Components/Search.js
 
-class Search extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = { fims: [] } // on initialise notre state avec un tableau film vide 
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+
+class Search extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+    this.state = {
+      films: []
     }
-    _loadFilms() { //  récupérer nos films dans ce tableau
-        getFilmsFromApiWithSearchedText("star").then(data => {
-          this.setState({ films: data.results }) // Nous avons modifié les données de notre component Search, le state, en passant par la fonction  setState 
-          //this.state.films  contient la liste des films de l'API. Votre FlatList utilise ces nouvelles données et les affiche.
-        })
-     }
-    render() {
-        
-        return (
-            <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film' />
-                {/* méthode au clic sur le bouton "Rechercher" */}
-                <Button title='Rechercher' onPress={() => this._loadFilms()} />  
-                <FlatList
-                    data={this.state.films} // On indique à notre flatlist d'utiliser la donnée pour afficher les films 
-                    keyExtractor={(item) => item.id.toString()} // identifie de manière unique nos items et crée une "key"
-                    renderItem={({item}) => <FilmItem film={item}/>} // on initialise nos items FilmItem, ajoutez notre propre prop film  
-                />
-            </View>
-        )
+  }
+
+  _loadFilms() {
+    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          this.setState({ films: data.results })
+      })
     }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+  }
+
+  render() {
+    console.log("RENDER")
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        marginTop: 20
-    },
-    textinput: {
-        marginLeft: 5,
-        marginRight: 5,
-        height: 50,
-        borderColor: '#000000',
-        borderWidth: 1,
-        paddingLeft: 5
-    }
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
 })
 
-export default Search 
+export default Search

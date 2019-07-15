e1c94b2fe95181bdd3405b0e1cfe70d8b5c0e56d
laralcq <laraloicq@gmail.com>
2019-07-15 14:51:31 +0200

    gestion de page

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -19,6 +19,17 @@ class Search extends React.Component {
     }
   }
 
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: []
+    }, () => { 
+      console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+      this._loadFilms() // permet de remettre à 0 
+  })
+}
+
   _loadFilms() {
     if (this.searchedText.length > 0) {
       this.setState({ isLoading: true })
@@ -56,9 +67,9 @@ class Search extends React.Component {
           style={styles.textinput}
           placeholder='Titre du film'
           onChangeText={(text) => this._searchTextInputChanged(text)}
-          onSubmitEditing={() => this._loadFilms()} // permet de valider la recherche avec le clavier du smartphone 
+          onSubmitEditing={() => this._searchFilms()} // permet de valider la recherche avec le clavier du smartphone 
         />
-        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
